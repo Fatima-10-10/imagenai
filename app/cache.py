7f3b6f01687e5ac9212561cd4e47bc78ea574753
Fatima-10-10 <fskhanh2015@gmail.com>
2026-05-10 21:57:48 +0500

    upgrade to persistent disk caching using shelve

--- app/cache.py
+++ app/cache.py
@@ -1,17 +1,20 @@
+import shelve
 from datetime import datetime, timedelta
 
-cache_store = {}
+CACHE_FILE = "cache_store"
 CACHE_EXPIRY_MINUTES = 60
 
 def get_from_cache(key: str):
-    if key in cache_store:
-        data, expiry = cache_store[key]
-        if datetime.now() < expiry:
-            return data
-        else:
-            del cache_store[key]
+    with shelve.open(CACHE_FILE) as db:
+        if key in db:
+            data, expiry = db[key]
+            if datetime.now() < expiry:
+                return data
+            else:
+                del db[key]
     return None
 
 def set_in_cache(key: str, value):
     expiry = datetime.now() + timedelta(minutes=CACHE_EXPIRY_MINUTES)
-    cache_store[key] = (value, expiry)
+    with shelve.open(CACHE_FILE) as db:
+        db[key] = (value, expiry)
